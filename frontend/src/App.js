ad3c2a1f1d85bb2951cd938b903d7550c345edce
ashishpokhrel1227 <ashishpkhrel27@gmail.com>
2020-11-13 19:59:46 +0545

    loader is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Cropper from 'react-cropper'
 import FileSaver from 'file-saver'
 
 import 'cropperjs/dist/cropper.css'
+import Loader from './Loader'
 
 import './App.css'
 
@@ -15,6 +16,7 @@ const App = () => {
   const [ cropper, setCropper ] = useState()  
   const [ segImage, setSegImage ] = useState(null)
   const [ foreground, setForeground ] = useState(null)
+  const [ loading, setLoading ] = useState(false)
 
   const onChange = (e) => {
     e.preventDefault()
@@ -36,6 +38,7 @@ const App = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
+    setLoading(true)
     let formData = new FormData()
     
     if (cropData !== '#') {
@@ -53,6 +56,7 @@ const App = () => {
     .then(res => {
       // console.log(res.data.image)
       setSegImage(res.data.image)
+      setLoading(false)
       console.log('foreground image is: ', foreground)
     })
     .catch(err => console.log(err))
@@ -89,113 +93,121 @@ const App = () => {
           <button type='submit'>Get Segmented Image</button>
         }
       </form>
-      <br />
-      <div className='image'>
-        
-        {
-          toCrop && 
-          !foreground &&
-          <div className='cropping-image'>
-            <Cropper 
-              style={{ height: 400, width: 400 }}
-              initialAspectRatio={1}
-              // preview='.img-preview'
-              src={URL.createObjectURL(image)}
-              viewMode={1}
-              guides={true}
-              minCropBoxHeight={10}
-              minCropBoxWidth={10}
-              background={false}
-              responsive={true}
-              autoCropArea={1}
-              checkOrientation={false}
-              onInitialized={(instance) => {
-                setCropper(instance)
-              }}
-            /> 
-          </div>
-        }
-        {
-          image && 
-          !toCrop && 
-          <div className='preview-image'>
-            <img 
-              src={URL.createObjectURL(image)}
-              style={{ width: 'auto', height: 400}}
-              alt='preview'
-            />
-          </div>
-        }
-        {
-          toCrop &&
-          cropData !== '#' &&
-          <div className='cropped-image'>
-            <img 
-              src={cropData}
-              style={{ width: 'auto', height: 400}}
-              alt='cropped'
-            />
-          </div>
-        }
-        {
-        segImage &&
-        <div className='segmented-image'>
-          <img 
-            src={`http://127.0.0.1:8000${segImage}`}
-            style={{ width: 'auto', height: 400}}
-            alt='segmentedImage'
-          />
-        </div>
-      }
       {
-        foreground &&
-        <div className='foreground-image' style={{ marginLeft: '5px'}}>
-          <img
-            src={`http://127.0.0.1:8000${foreground}`} 
-            style={{ width: 'auto', height: 400 }}
-            alt='foregroundImage'
-          />
-        </div>
+        loading ?
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+            <Loader></Loader>
+          </div>
+        :
+          <React.Fragment>
+            <div className='image'>  
+              {
+                toCrop && 
+                !foreground &&
+                <div className='cropping-image'>
+                  <Cropper 
+                    style={{ height: 400, width: 400 }}
+                    initialAspectRatio={1}
+                    // preview='.img-preview'
+                    src={URL.createObjectURL(image)}
+                    viewMode={1}
+                    guides={true}
+                    minCropBoxHeight={10}
+                    minCropBoxWidth={10}
+                    background={false}
+                    responsive={true}
+                    autoCropArea={1}
+                    checkOrientation={false}
+                    onInitialized={(instance) => {
+                      setCropper(instance)
+                    }}
+                  /> 
+                </div>
+              }
+              {
+                image && 
+                !toCrop && 
+                <div className='preview-image'>
+                  <img 
+                    src={URL.createObjectURL(image)}
+                    style={{ width: 'auto', height: 400}}
+                    alt='preview'
+                  />
+                </div>
+              }
+              {
+                toCrop &&
+                cropData !== '#' &&
+                <div className='cropped-image'>
+                  <img 
+                    src={cropData}
+                    style={{ width: 'auto', height: 400}}
+                    alt='cropped'
+                  />
+                </div>
+              }
+              {
+              segImage &&
+              <div className='segmented-image'>
+                <img 
+                  src={`http://127.0.0.1:8000${segImage}`}
+                  style={{ width: 'auto', height: 400}}
+                  alt='segmentedImage'
+                />
+              </div>
+              }
+              {
+                foreground &&
+                <div className='foreground-image' style={{ marginLeft: '5px'}}>
+                  <img
+                    src={`http://127.0.0.1:8000${foreground}`} 
+                    style={{ width: 'auto', height: 400 }}
+                    alt='foregroundImage'
+                  />
+                </div>
+              }
+            </div>
+            <br />
+            <div className='botton'>
+              {
+                segImage &&
+                <div className='foreground-botton'>
+                  <button
+                    onClick={getForegroundImage}
+                  >
+                    Get Foreground
+                  </button>
+                </div>
+              }
+              {
+                toCrop && 
+                // !foreground &&
+                <div className='enable-crop-botton'>
+                  <button 
+                    onClick={getCropData}
+                  >
+                    Crop
+                  </button>
+                </div>
+              }
+              <br />
+              {
+                image &&
+                !toCrop &&
+                <div className='crop-botton'>
+                  <button onClick={handleCrop}>Crop Image</button> 
+                </div>
+              }
+              {
+                foreground &&
+                <div className='download-botton' style={{ marginLeft: '5px'}}>
+                  <button onClick={downloadForeground}>Download foreground</button>
+                </div>
+              }
+            </div>
+        </React.Fragment>
       }
-      </div>
       <br />
-      <div className='botton'>
-        {
-          segImage &&
-          <div className='foreground-botton'>
-            <button
-              onClick={getForegroundImage}
-            >
-              Get Foreground
-            </button>
-          </div>
-        }
-        {
-          toCrop && 
-          // !foreground &&
-          <div className='enable-crop-botton'>
-            <button 
-              onClick={getCropData}
-            >
-              Crop
-            </button>
-          </div>
-        }
-        <br />
-        {
-          image &&
-          !toCrop &&
-          <div className='crop-botton'>
-            <button onClick={handleCrop}>Crop Image</button> 
-          </div>
-        }
-        {
-          foreground &&
-          <div className='download-botton' style={{ marginLeft: '5px'}}>
-            <button onClick={downloadForeground}>Download foreground</button>
-          </div>
-        }
-      </div>
     </div>
   )
 }
